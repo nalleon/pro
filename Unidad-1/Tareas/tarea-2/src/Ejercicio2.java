c63173d6b5e92e51d4059931909125fe61a61d09
nalleon <nabil14716@gmail.com>
2023-09-25 16:20:29 +0100

    Actualización de la tarea 2.

---
+++ Unidad-1/Tareas/tarea-2/src/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * @author nalleon
+ */
+public class Ejercicio2 {
+
+    /**
+     * Desarrolle un algoritmo que permita leer tres valores y almacenarlos en las variables A, B y C respectivamente. El algoritmo debe imprimir cual es el mayor y cual es el menor. Recuerde constatar que los tres valores introducidos por el teclado sean valores distintos. Presente un mensaje de alerta en caso de que se detecte la introducción de valores iguales.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
