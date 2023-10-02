e9b06a172467ec2e4fdc67dce23030864e051fc0
nalleon <nabil14716@gmail.com>
2023-10-02 16:25:59 +0100

    Ejercicios.

---
+++ Unidad-1/Tareas/tarea-2/src/main/java/ies/puerto/Ejercicio8.java
@@ -0,0 +1,40 @@
+package ies.puerto;
+import java.util.Scanner;
+
+
+/**
+ * Desarrolla un algoritmo que permita calcular el promedio de notas;
+ * finaliza cuando N = 0.
+ */
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner lecture = new Scanner(System.in);
+
+
+        int suma = 0;
+        int contador = 0;
+
+
+        System.out.print("Para finalizar introduzca 0. ");
+        while (true) {
+            System.out.print("Ingrese una nota:");
+            int nota = lecture.nextInt();
+
+            if (nota == 0) {
+                break; // Salir del bucle si se ingresa 0
+            }
+
+
+            suma += nota;
+            contador++;
+        }
+
+
+        if (contador > 0) {
+            double promedio = (double) suma / contador;
+            System.out.println("El promedio de las notas es: " + promedio);
+        } else {
+            System.out.println("No se ingresaron notas.");
+        }
+    }
+}
