da8fd78f751f6e45af4c7feaaa8e58736176d698
unknown <nabil14716@gmail.com>
2023-11-27 10:52:56 +0000

    Se ha actualizado el contenido

--- Unidad-3/Tareas/estructura-de-repetición/src/main/java/ies/puerto/Ejercicio2.java
+++ Unidad-3/Tareas/estructura-de-repetición/src/main/java/ies/puerto/Ejercicio2.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        boolean prime;
         for (int i=100; i <=1000; i++){
             if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0){
                 System.out.println("The number is " + i);
