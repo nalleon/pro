7ac927d2e8634a41517e8e629d34f9c7ed444805
nalleon <nabil14716@gmail.com>
2024-01-18 14:36:15 +0000

    Solucion de problema de fusion

--- Unidad-3/Tareas/herencia-ejercicios/src/test/java/ies/puerto/PersonaTest.java
+++ Unidad-3/Tareas/herencia-ejercicios/src/test/java/ies/puerto/PersonaTest.java
@@ -3,7 +3,7 @@ package ies.puerto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Ejercicio1Test {
- Ejercicio1 ejercicio1 = new Ejercicio1();
+public class PersonaTest {
+ Persona persona = new Persona ();
 
 }
