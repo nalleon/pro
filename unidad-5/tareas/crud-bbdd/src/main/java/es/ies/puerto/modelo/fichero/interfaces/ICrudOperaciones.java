0f582ee4fd2f1e3d73c1f85a1e07d4153aa126f6
nalleon <nabil14716@gmail.com>
2024-04-01 15:24:27 +0100

    Se comienzan las operaciones con bbdd

---
+++ unidad-5/tareas/crud-bbdd/src/main/java/es/ies/puerto/modelo/fichero/interfaces/ICrudOperaciones.java
@@ -0,0 +1,12 @@
+package es.ies.puerto.modelo.fichero.interfaces;
+
+import java.util.List;
+
+public interface ICrudOperaciones {
+    List<Character> obtainCharacters();
+    Character obtainCharacter(Character character);
+    void addCharacter(Character character);
+    void deleteCharacter(Character character);
+    void updateCharacter(Character character);
+    void updateFile(List<Character> characters);
+}
