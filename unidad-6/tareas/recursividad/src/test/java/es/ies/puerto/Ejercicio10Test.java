7cb41fc15125d15e1fa96c09d45c15ed17c6f83b
nalleon <nabil14716@gmail.com>
2024-04-11 16:23:32 +0100

    Se añade la practica de recursividad

---
+++ unidad-6/tareas/recursividad/src/test/java/es/ies/puerto/Ejercicio10Test.java
@@ -0,0 +1,19 @@
+package es.ies.puerto;
+
+import es.ies.puerto.utilities.Utilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Ejercicio10Test extends Utilities {
+    Ejercicio10 ejercicio;
+    String result = "[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]";
+    @BeforeEach
+    public void beforeEach() {
+    ejercicio = new Ejercicio10();
+    }
+
+    @Test
+    public void ejercicio1Test() {
+
+    }
+}
---
+++ unidad-6/tareas/recursividad/src/test/java/es/ies/puerto/utilities/Utilities.java
@@ -0,0 +1,6 @@
+package es.ies.puerto.utilities;
+
+public class Utilities {
+    public static final String MESSAGE_ERROR = "EXPECTED RESULT NOT FOUND";
+
+}
