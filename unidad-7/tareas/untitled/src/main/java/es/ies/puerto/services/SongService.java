8de6c79e1d38187722c067e3f4c0090380cb3eca
nalleon <nabil14716@gmail.com>
2024-05-13 15:30:16 +0100

    Se empieza la practica

---
+++ unidad-7/tareas/untitled/src/main/java/es/ies/puerto/services/SongService.java
@@ -0,0 +1,20 @@
+package es.ies.puerto.services;
+
+import es.ies.puerto.models.Song;
+import es.ies.puerto.repositories.SongRepository;
+
+import javax.ws.rs.*;
+
+@Path("/")
+@Consumes({"application/json"})
+@Produces({"application/json"})
+public class SongService {
+    private SongRepository songRepository = new SongRepository();
+    @GET
+    @Path("/{id}")
+    @Consumes({"application/json"})
+    @Produces({"application/json"})
+    public Song getSongById(@PathParam("id") String id) {
+        return songRepository.getSongById(id);
+    }
+}
