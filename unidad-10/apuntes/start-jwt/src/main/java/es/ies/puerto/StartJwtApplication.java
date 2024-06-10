7915ec573f61252b4e54dc815291230720068957
nalleon <nabil14716@gmail.com>
2024-06-10 18:18:05 +0100

    Se añaden archivos varios

---
+++ unidad-10/apuntes/start-jwt/src/main/java/es/ies/puerto/StartJwtApplication.java
@@ -0,0 +1,13 @@
+package es.ies.puerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartJwtApplication.class, args);
+	}
+
+}
